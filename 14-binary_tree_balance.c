9601f032d4d472d7c4248c5f4bfaf5a34584ddce
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-28 23:55:11 +0100

    Task 14 : measures the balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,6 +8,9 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
@@ -19,5 +22,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+	return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
