e184f9af78167b3b2d5e778dc8c65ce4b0a508ab
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-28 19:18:16 +0100

    Task 8 : binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,3 +5,12 @@
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		free(tree->n);
+	}
+}
