7886d908916624eec285ce878cb155049aeeb034
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-28 14:48:11 +0100

    task 0 : creating a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,5 +9,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	new_node->n = value;
+
+	return (new_node);
 }
 
