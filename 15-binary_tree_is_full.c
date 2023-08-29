ccc8926a32de61d0be042402615e4e073ace5e66
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-29 16:04:42 +0100

    xor let us know about the balance

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,16 +8,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-}
+	int full = 1;
 
-/**
- * is_full_recursive - Checks if a binary tree is full recursively.
- *
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 0 if tree is not NULL, otherwise 1.
- */
-int is_full_recursive(const binary_tree_t *tree)
-{
+	if (tree)
+	{
+		if ((!tree->left && tree->right) || (tree->left && !tree->right))
+			return (0);
+		full = binary_tree_is_full(tree->left);
+		full = binary_tree_is_full(tree->right);
+	}
+	return (full);
 }
-
