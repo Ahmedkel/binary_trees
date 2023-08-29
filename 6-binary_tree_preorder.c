0d02addcb9360ae31eb16c58a4670fa00978d309
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-29 10:09:52 +0000

    fixing

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,7 +9,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		free(tree->n);
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
