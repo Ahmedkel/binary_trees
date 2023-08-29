54931d5e43928b5c3d74f8574189eb3a84dec30e
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-29 18:10:40 +0100

    who is my ancle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,5 +8,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	return (node->parent->parent->right == node->parent ?
+	node->parent->parent->left : node->parent->parent->right);
 }
 
