58b08f560e41c82fa5e4c3074218ab3b12040491
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-29 13:59:18 +0100

    count the size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,5 +8,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += binary_tree_size(tree->left);
+		size++;
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
 }
 
